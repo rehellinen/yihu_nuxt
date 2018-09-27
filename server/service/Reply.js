0db1253059a20fb12c150099af83a0467960e735
rehellinen <912377791@qq.com>
2018-09-27 10:34:03 +0800

    tpl类优化

--- server/service/Reply.js
+++ server/service/Reply.js
@@ -3,9 +3,20 @@
  *  Create By rehellinen
  *  Create On 2018/9/26 10:12
  */
-const tip = '爱你哟~'
+import config from '../utils/config'
+let {msgType} = config
+const tip = '爱你哟~1111'
 
 export default async (ctx, next) => {
   ctx.weixin.content = tip
-  ctx.body = tip
+  let weixin = ctx.weixin
+
+  // 文本消息
+  if (weixin.MsgType === msgType.TEXT) {
+    weixin.content = tip
+  } else if (weixin.MsgType === msgType.EVENT) {
+    if (weixin.Event === 'subscribe') {
+      
+    }
+  }
 }
