ea7bb4b1c2fce1ef9db30f8e211732cdedcd8881
rehellinen <912377791@qq.com>
2018-09-27 10:49:05 +0800

    关注发送消息

--- server/service/Reply.js
+++ server/service/Reply.js
@@ -11,12 +11,16 @@ export default async (ctx, next) => {
   ctx.weixin.content = tip
   let weixin = ctx.weixin
 
-  // 文本消息
+
   if (weixin.MsgType === msgType.TEXT) {
+    // 文本消息
     weixin.content = tip
   } else if (weixin.MsgType === msgType.EVENT) {
+    // 事件
     if (weixin.Event === 'subscribe') {
-      
+      weixin.content = '欢迎关注我们~'
+    } else if (weixin.Event === 'unsubscribe') {
+
     }
   }
 }
